68611511bf3aa502c8ce905bc669246c7aa387cf
sfaenza <cinzia.faenza@gmail.com>
2014-09-15 10:00:29 -0700

    update environments deploy config files

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+# role :app, %w{deploy@example.com}
+# role :web, %w{deploy@example.com}
+# role :db,  %w{deploy@example.com}
 
 
 # Extended Server Syntax
@@ -15,7 +15,10 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'dash-stg.cdlib.org', user: 'dash', roles: %w{web app}
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/apps/dash/apache/htdocs/dash-stg'
 
 
 # Custom SSH Options
