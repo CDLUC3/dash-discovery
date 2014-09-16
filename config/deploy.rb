843864b9975195bd9fb83a18e2b3d43017a43954
sfaenza <cinzia.faenza@gmail.com>
2014-09-16 14:59:49 -0700

    set deploy branch to stage

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,8 @@ set :application, 'dash-discovery'
 set :repo_url, 'git@github.com:CDLUC3/dash-discovery.git'
 
 # Default branch is :master
-set :branch, 'master'
+# set :branch, 'master'
+set :branch, 'stage'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
